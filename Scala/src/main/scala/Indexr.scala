39d3b661a2e28eae292281444caad03cbccf43d0
Brenden Grace <brenden.grace@gmail.com>
2011-05-02 06:33:58 -0400

    Use christophsturm's trove impl.

--- Scala/src/main/scala/Indexr.scala
+++ Scala/src/main/scala/Indexr.scala
@@ -1,27 +1,33 @@
-import scala.collection.mutable.Set
+//import scala.collection.mutable.Set
+import gnu.trove.set.TIntSet
+import gnu.trove.set.hash.TIntHashSet
+
 
 object Indexr extends Application {
 
-  var one = Set(1)
-  var two = Set(1)
+  val one = new TIntHashSet(100000);
+  val two = new TIntHashSet(100000);
 
   var t = System.currentTimeMillis
 
-  for (i <- 0.until(9999999)) {
-    one += i
+  for (i <- 0.until(99999)) {
+    one.add(i)
   }
 
   Console.println("Loaded set in " + ((System.currentTimeMillis-t)/ 1000.0))
 
   t = System.currentTimeMillis
 
-  two = one.clone
+  for (i <- 0.until(99999)) {
+    two.add(i)
+  }
 
   Console.println("Loaded set in " + ((System.currentTimeMillis-t)/ 1000.0))
 
   t = System.currentTimeMillis
-  val intersects = one intersect two
 
-  Console.println(intersects.size + " intersects in " + ((System.currentTimeMillis-t)/ 1000.0));
+  two.retainAll(one)
+
+  Console.println(two.size() + " intersects in " + ((System.currentTimeMillis-t)/ 1000.0));
 }
 
